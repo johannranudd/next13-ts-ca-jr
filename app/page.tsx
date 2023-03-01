6076a6aa2af2ad87a365f93db67b8398a6654ea5
johannranudd <33978097+johannranudd@users.noreply.github.com>
2023-03-02 00:10:06 +0100

    re-adds Suspense for deployment

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,13 @@
-// import { Suspense } from "react";
+import { Suspense } from "react";
 import ListOfProducts from "./components/lists/ListOfProducts";
 
 export default function Home() {
   return (
     <main className="mb-28">
       <h1>Home Page</h1>
-      {/* <Suspense fallback={<h2>Loading...</h2>}> */}
-      <ListOfProducts />
-      {/* </Suspense> */}
+      <Suspense fallback={<h2>Loading...</h2>}>
+        <ListOfProducts />
+      </Suspense>
     </main>
   );
 }
