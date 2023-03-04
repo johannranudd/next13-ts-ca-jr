d66473a96d7f562287b6d78ab8f180e56bc56e48
johannranudd <33978097+johannranudd@users.noreply.github.com>
2023-03-04 17:31:25 +0100

    hydration error in themeProvider fixed

--- app/context/colorThemeProvider.tsx
+++ app/context/colorThemeProvider.tsx
@@ -1,6 +1,5 @@
 "use client";
 import { ThemeProvider } from "next-themes";
-import Script from "next/script";
 import { useEffect, useState } from "react";
 
 const ColorThemeProvider = ({ children }: { children: React.ReactNode }) => {
