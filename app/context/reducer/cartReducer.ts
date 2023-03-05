affca8990af934306eeb34c93642f343663117ed
johannranudd <33978097+johannranudd@users.noreply.github.com>
2023-03-05 04:36:16 +0100

    continue working on reducer

--- app/context/reducer/cartReducer.ts
+++ app/context/reducer/cartReducer.ts
@@ -22,10 +22,48 @@ export const initialState: IState = {
 export function reducer(state: IState, action: any) {
   switch (action.type) {
     case "ADD":
-      return {
-        ...state,
-        products: [...state.products, action.payload],
-      };
+      // console.log(action.payload);
+      const locStor = getItem("cart");
+      if (state.products.length === 0) {
+        console.log("no Locstor");
+        return {
+          ...state,
+          products: [...state.products, { ...action.payload, amountInCart: 0 }],
+        };
+      } else {
+        const mappy = state.products.map((item: any) => {
+          if (item.id === action.payload.id) {
+            console.log("equal");
+            return { ...item, amountInCart: item.amountInCart + 1 };
+            // return {
+            //   ...state,
+            //   products: [
+            //     ...state.products,
+            //     { ...item, amountInCart: item.amountInCart + 1 },
+            //   ],
+            // };
+          } else {
+            console.log("unique");
+            return { ...item, amountInCart: 0 };
+            // return {
+            //   ...state,
+            //   products: [...state.products, { ...item, amountInCart: 0 }],
+            // };
+          }
+          // return item;
+        });
+        console.log(mappy);
+        // console.log(state.products);
+        // const newArray = [...state.products, mappy[0]];
+        // console.log(newArray);
+        // setItem("cart", newArray);
+        return {
+          ...state,
+          products: [...state.products, mappy[0]],
+        };
+        // const newPayload = { ...action.payload, amountInCart: 0 };
+      }
+
     case "CHANGE":
     case "DELETE":
     default: {
@@ -34,3 +72,29 @@ export function reducer(state: IState, action: any) {
     }
   }
 }
+
+// const locStor = getItem("cart");
+//       if (locStor.length === 0) {
+//         console.log("no Locstor");
+//         return {
+//           ...state,
+//           products: [...state.products, { ...action.payload, amountInCart: 0 }],
+//         };
+//       } else {
+//         const mappy = locStor.map((item: any) => {
+//           if (item.id === action.payload.id) {
+//             console.log("equal");
+//             return { ...item, amountInCart: (item.amountInCart += 1) };
+//           } else {
+//             console.log("unique");
+//             return { ...action.payload, amountInCart: 0 };
+//           }
+//         });
+//         console.log(mappy[0]);
+//         return {
+//           ...state,
+//           products: [...state.products, { ...mappy[0] }],
+//         };
+//         // const newPayload = { ...action.payload, amountInCart: 0 };
+//       }
+//     // return state;
