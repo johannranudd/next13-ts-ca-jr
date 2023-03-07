b2b183d4f7e0880f8fd92131650de51fa5cc4713
johannranudd <33978097+johannranudd@users.noreply.github.com>
2023-03-07 22:47:05 +0100

    adds a rating star system

--- app/products/[productId]/page.tsx
+++ app/products/[productId]/page.tsx
@@ -3,6 +3,8 @@ import { IDataObject } from "@/types/types";
 import Image from "next/image";
 import { use } from "react";
 import { IReviews } from "@/types/types";
+import PriceComponent from "@/app/cart/PriceComponent";
+import RatingComponent from "./RatingComponent";
 
 interface IParams {
   params: {
@@ -24,12 +26,10 @@ export default function ProductDetailPage({ params: { productId } }: IParams) {
     reviews,
   } = data;
 
-  // console.log(reviews);
   return (
     <div key={id} className="mb-28 mt-20 bg-[#def]">
       <h1>{title}</h1>
-      <div className="w-[90%] max-w-[400px] mx-auto bg-[#edf]">
-        {/* image */}
+      <div className="w-[90%] max-w-[400px] mx-auto bg-[#edf] mb-4">
         <div className="relative h-64 w-full">
           <Image
             src={imageUrl}
@@ -38,44 +38,73 @@ export default function ProductDetailPage({ params: { productId } }: IParams) {
             className="object-cover"
           />
         </div>
-        {/* description */}
-        <div>{description}</div>
-        {/* prices */}
-        <div>
-          <p>discountedPrice: {discountedPrice}</p>
-          <p>price: {price}</p>
+        <div className="p-2 space-y-3">
+          <div>{description}</div>
+          <SingleItemPriceComponent {...data} />
+          <RatingComponent rating={rating} />
+          <TagsComponent tags={tags} />
         </div>
-        {/* tags and rating */}
-        <div>
-          <p>rating: {rating}</p>
-          {tags && (
-            <p>
-              Tags:{" "}
-              {tags?.map((tag: string, index) => {
-                return (
-                  <span key={index}>
-                    {tag}
-                    {index < tags.length - 1 && ", "}
-                  </span>
-                );
-              })}
-            </p>
-          )}
-        </div>
-        {/* reviews */}
-        {reviews.length > 0 && <ReviewsComponent reviews={reviews} />}
       </div>
+      <ReviewsComponent reviews={reviews} />
     </div>
   );
 }
 
 // todo: generateStaticParams to prefetch all potential Ids
 
-function ReviewsComponent({ reviews }: any) {
-  console.log(reviews);
+function TagsComponent({ tags }: any) {
+  if (tags.length === 0) return <div></div>;
   return (
-    <ul>
-      <li>assdasd</li>
-    </ul>
+    <div>
+      {tags && (
+        <p>
+          {tags?.map((tag: string, index: number) => {
+            return <small key={index}>#{`${tag} `}</small>;
+          })}
+        </p>
+      )}
+    </div>
+  );
+}
+
+function SingleItemPriceComponent({ discountedPrice, price }: IDataObject) {
+  return (
+    <div>
+      <p className="inline">Price </p>
+      {discountedPrice < price ? (
+        <>
+          <strong>
+            {discountedPrice}{" "}
+            <small className="line-through text-gray-500">{price}</small>
+          </strong>
+          <p>
+            Save: <strong>{(price - discountedPrice).toFixed(2)}</strong>
+          </p>
+        </>
+      ) : (
+        <strong>{price}</strong>
+      )}
+    </div>
+  );
+}
+
+function ReviewsComponent({ reviews }: any) {
+  if (reviews.length === 0) return <div></div>;
+  return (
+    <div className="w-[90%] max-w-[400px] mx-auto space-y-4">
+      <h3>Reviews</h3>
+      <ul>
+        {reviews.map((review: IReviews) => {
+          const { id, username, rating, description } = review;
+          return (
+            <li key={id} className="bg-[#fed]">
+              <p>{username}</p>
+              <p>{rating}</p>
+              <p>{description}</p>
+            </li>
+          );
+        })}
+      </ul>
+    </div>
   );
 }
