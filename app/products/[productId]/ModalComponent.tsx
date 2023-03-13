53b2ad21b5b93d3250fd1c8970e5d60cc8b7a753
johannranudd <33978097+johannranudd@users.noreply.github.com>
2023-03-13 22:20:36 +0100

    more styling

--- app/products/[productId]/ModalComponent.tsx
+++ app/products/[productId]/ModalComponent.tsx
@@ -5,28 +5,26 @@ export default function ModalComponent({ title, imageUrl }: any) {
   const [modalIsOpen, setModalIsOpen] = useState(false);
 
   return (
-    <>
-      <div
-        role="button"
-        onClick={() => setModalIsOpen(!modalIsOpen)}
-        className={`group absolute top-0 left-0 z-40 h-full w-full grid place-items-center hover:bg-[#000000bb]`}
-      >
-        <p className="text-primary opacity-0 group-hover:opacity-100">
-          View Image
-        </p>
-        {modalIsOpen && (
-          <div className="bg-[#000000cc] w-screen h-screen fixed top-0 left-0">
-            <div className="fixed top-1/2 left-1/2 -translate-y-1/2 -translate-x-1/2 z-50 h-[80%] w-[95%] max-w-[700px]">
-              <Image
-                src={imageUrl}
-                alt={`image of ${title}`}
-                fill={true}
-                className="object-contain"
-              />
-            </div>
+    <div
+      role="button"
+      onClick={() => setModalIsOpen(!modalIsOpen)}
+      className={`group absolute top-0 left-0 z-40 h-full w-full grid place-items-center hover:bg-[#000000bb]`}
+    >
+      <p className="text-primary opacity-0 group-hover:opacity-100">
+        View Image
+      </p>
+      {modalIsOpen && (
+        <div className="bg-[#000000cc] w-screen h-screen fixed top-0 left-0">
+          <div className="fixed top-1/2 left-1/2 -translate-y-1/2 -translate-x-1/2 z-50 h-[80%] w-[95%] max-w-[700px]">
+            <Image
+              src={imageUrl}
+              alt={`image of ${title}`}
+              fill={true}
+              className="object-contain"
+            />
           </div>
-        )}
-      </div>
-    </>
+        </div>
+      )}
+    </div>
   );
 }
